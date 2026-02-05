b0af1c6fb3f8dc7905cc24f9ca003d8a26ff104a
Elis Bonner <bonner@chalmers.se>
2026-02-05 14:10:43 +0100

    Test skapat YOLO

---
+++ test/ScaniaTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.awt.Color;
+
+class ScaniaTest {
+    private Scania scania1;
+    @BeforeEach
+    void setup(){
+        scania1 = new Scania();
+    }
+
+    @Test
+    void raiseBedAngle() {
+
+    }
+
+    @Test
+    void lowerBedAngle() {
+    }
+}
