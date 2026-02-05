96524b37033071f347c304030c96c17afb3af0d6
Elis Bonner <bonner@chalmers.se>
2026-02-05 14:05:06 +0100

    qweqwe

--- Scania.java
+++ Scania.java
@@ -1,34 +1,21 @@
 import java.awt.*;
 
-public class Scania {
+public class Scania extends Car {
 
-    private int nrDoors; // Number of doors on the car
-    private double enginePower; // Engine power of the car
-    private double currentSpeed; // The current speed of the car
-    private Color color; // Color of the car
-    private String modelName; // The car model name
-    private double x_value = 0;
-    private double y_value = 0;
-    private int direction = 0;
-    private int bedAngle;
+    public final static double trimFactor = 1.25;
+    private int bedAngle = 0;
 
-    public Scania(){
+    public Scania() {
+        super(2, 200, Color.white, "Scania", 0 , 0);
+    }
 
-        nrDoors = 4;
-        enginePower = 100;
-        currentSpeed = 0;
-        color = Color.black;
-        modelName = "Scania";
-        x_value = 0;
-        y_value = 0;
-        direction = 0;
-        bedAngle = 0;
-
+    public double speedFactor(){
+        return super.getEnginePower() * 0.01 * trimFactor;
     }
 
     public void raiseBedAngle (int angleIncrease) {
         int newAngle = bedAngle + angleIncrease;
-        if (currentSpeed == 0) {
+        if (super.getCurrentSpeed() == 0) {
             if (newAngle <= 70) {
                 bedAngle = newAngle;
             }
@@ -43,7 +30,7 @@ public class Scania {
 
     public void lowerBedAngle (int angleDecrease) {
         int newAngle = bedAngle - angleDecrease;
-        if (currentSpeed == 0) {
+        if (super.getCurrentSpeed() == 0) {
             if (newAngle >= 0) {
                 bedAngle = newAngle;
             }
