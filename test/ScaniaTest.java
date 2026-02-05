2ccca1c11373faf0e14030e7e85d84de8de575e4
augustkl <kaggbac@gmail.com>
2026-02-05 14:27:24 +0100

    ScaniaTest

--- test/ScaniaTest.java
+++ test/ScaniaTest.java
@@ -12,10 +12,22 @@ class ScaniaTest {
 
     @Test
     void raiseBedAngle() {
-
+        scania1.raiseBedAngle(10);
+        assertEquals(10, scania1.getBedAngle());
+        scania1.raiseBedAngle(70);
+        assertEquals(10, scania1.getBedAngle());
+        scania1.raiseBedAngle(20);
+        assertEquals(30, scania1.getBedAngle());
     }
 
     @Test
     void lowerBedAngle() {
+        scania1.raiseBedAngle(55);
+        scania1.lowerBedAngle(10);
+        assertEquals(45, scania1.getBedAngle());
+        scania1.lowerBedAngle(70);
+        assertEquals(45, scania1.getBedAngle());
+        scania1.lowerBedAngle(20);
+        assertEquals(25, scania1.getBedAngle());
     }
 }
