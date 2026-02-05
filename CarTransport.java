6567cb11772ec7caebb95cb9de6dc3118a95ac3e
Elis Bonner <bonner@chalmers.se>
2026-02-05 15:09:40 +0100

    BiTNosdfndghjkl

--- CarTransport.java
+++ CarTransport.java
@@ -1,5 +1,14 @@
-public class CarTransport {
+import java.awt.*;
 
+public class CarTransport extends Car {
 
+    public final static double trimFactor = 1.25;
 
+    public CarTransport() {
+        super(2, 250, Color.blue, "Biltransport", 0 , 0);
+    }
+
+    public double speedFactor(){
+        return super.getEnginePower() * 0.01 * trimFactor;
+    }
 }
---
+++ specialCar.java
@@ -0,0 +1,14 @@
+public class specialCar {
+    //attributes
+    private int bedAngle;
+
+
+    public specialCar(int bedAngle){
+        this.bedAngle = bedAngle;
+    }
+
+
+    //Getters
+    public int getBedAngle(){return bedAngle; }
+
+}
